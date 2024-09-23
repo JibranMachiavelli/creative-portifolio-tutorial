c301c56b265da7eab46e6aafa350e0ce633fa852
JibranMachiavelli <jibranind31@gmail.com>
2024-09-22 23:17:27 -0300

    feat: implementado no tag animação

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
@@ -34,12 +34,25 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           ease: "elastic.out(1,0.3)",
           duration: 1,
           transformOrigin: "left top",
+          delay: 0.5,
           stagger: {
             each: 0.1,
-            from: "random"
-          }
+            from: "random",
+          },
         }
       );
+
+      tl.fromTo(".job-title", {
+        y: 20,
+        opacity: 0,
+        scale: 1.2,
+      },{
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        scale: 1,
+        ease: "elastic.out(1,0.3)",
+      });
     }, component);
     return () => ctx.revert();
   }, []);
@@ -67,7 +80,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
         <div className="col-start-1 md:row-start-1">
           <h1
-            className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
+            className="mb-8 text-[clamp(2rem,10vmin,10rem)] font-extrabold leading-none tracking-tighter"
             aria-label={
               slice.primary.first_name + " " + slice.primary.last_name
             }
@@ -79,7 +92,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
               {renderLetters(slice.primary.last_name, "first")}
             </span>
           </h1>
-          <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl">
+          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
             {slice.primary.tag_line}
           </span>
         </div>
